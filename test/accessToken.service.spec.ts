b4e1904f1b6a62d3789f51ebdc8f481e0b20cd16
rimu boddy <rimu@orbica.world>
2019-10-03 14:12:13 +1300

    add a test

---
+++ test/accessToken.service.spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AccessTokenService } from '../src/accessToken/accessToken.service';
+import { AccessTokenController } from '../src/accessToken/accessToken.controller';
+import { WorkerService } from '../src/worker/worker.service';
+import { WorkerProvider } from '../src/worker/worker.provider';
+import { AccessTokenModule } from '../src/accessToken/accessToken.module';
+import { AppModule } from '../src/app.module';
+import { WorkerModule } from '../src/worker/worker.module';
+import { DatabaseModule } from '../src/db/database.module';
+import { mockPost, mockGet } from './httpUtils';
+import testconstants from './test-constants';
+import { genLocationTimestamp } from './dataGenerators';
+
+
+describe('tests the Access Token', () => {
+
+  let accService: AccessTokenService;
+
+  beforeEach(async () => {
+    console.log(accService, '#$%#^#$%&#$&#$%&#$%&#$%^&@$%^@$%^@^$')
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule, AccessTokenModule, WorkerModule],
+    }).compile();
+    accService = app.get<AccessTokenService>(AccessTokenService);
+    console.log(accService, '#$%#^#$%&#$&#$%&#$%&#$%^&@$%^@$%^@^$')
+  });
+
+  describe('test up the accessToken service', () => {
+
+    it('creates an access token', async () => {
+      const token = await accService.createAccessToken(3);//OUR 3RD WORKER IS THE WORKER
+      expect(token).toBe("234234234243");
+    });
+
+  });
+
+});
+
