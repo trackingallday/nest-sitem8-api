a8a8ceadca1cfc1cf0ac1c34f3452d7978e4bb3a
rimu-at-307937472747 <rimuboddy@Rimus-iMac.local>
2019-10-03 16:24:03 +1300

    add a test for the loc service methods that are not used by the controller

---
+++ test/locationTimestamp.service.spec.ts
@@ -0,0 +1,76 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LocationTimestampService } from '../src/locationTimestamp/locationTimestamp.service';
+import { LocationTimestampModule } from '../src/locationTimestamp/locationTimestamp.module';
+import { DatabaseModule } from '../src/db/database.module';
+import { All } from '@nestjs/common';
+import moment = require('moment');
+
+
+let mockCreatePassword = jest.fn();
+mockCreatePassword.mockReturnValue("password");
+
+jest.mock('../src/common/global', function() {
+  return { createPassword: jest.fn().mockImplementation(() => {
+    return mockCreatePassword();
+  })
+  }
+});
+
+
+describe('tests the Location Timstamp Service', () => {
+
+  let locService: LocationTimestampService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule, LocationTimestampModule],
+    }).compile();
+    locService = app.get<LocationTimestampService>(LocationTimestampService);
+  });
+
+  describe('test up the location service functions', () => {
+    
+    it('checks last onsite', async () => {
+      const allLocs = await locService.findAll();
+      const lastOnSite = await locService.getLastOnSite(3, 40);
+      const latestLoc = allLocs.sort((a, b) => {
+        return moment(a.locationDateTime).isAfter(moment(b.locationDateTime)) ? -1 : 1;
+      })[0];
+      expect(lastOnSite.closestSiteDistance).toBe(latestLoc.closestSiteDistance);
+      expect(lastOnSite.id).toBe(latestLoc.id);
+    });
+
+    it('checks findByCompanyIdDateRange', async () => {
+      const allLocs = await locService.findAll();
+      allLocs.sort((a, b) => {
+        return moment(a.locationDateTime).isAfter(moment(b.locationDateTime)) ? 1 : -1;
+      });
+      const startDate = allLocs[1].locationDateTime;
+      const endDate = allLocs[3].locationDateTime;
+      const inRange = await locService.findByCompanyIdDateRange(startDate, endDate, 1);
+      expect(inRange.length).toBe(3);
+      expect(allLocs[4].locationDateTime.getTime()).toBeGreaterThan(
+        inRange[inRange.length - 1].locationDateTime.getTime());
+      expect(allLocs[0].locationDateTime.getTime()).toBeLessThan(
+        inRange[0].locationDateTime.getTime());
+    });
+
+    it('checks findByDeviceIdDateRange', async () => {
+      const allLocs = await locService.findAllWhere({ where: { deviceId: '32q453453145345' }});
+      allLocs.sort((a, b) => {
+        return moment(a.locationDateTime).isAfter(moment(b.locationDateTime)) ? 1 : -1;
+      });
+      const startDate = allLocs[1].locationDateTime;
+      const endDate = allLocs[2].locationDateTime;
+      const inRange = await locService.findByDeviceIdDateRange(startDate, endDate, '32q453453145345');
+      expect(inRange.length).toBe(2);
+      expect(allLocs[3].locationDateTime.getTime()).toBeGreaterThan(
+        inRange[inRange.length - 1].locationDateTime.getTime());
+      expect(allLocs[0].locationDateTime.getTime()).toBeLessThan(
+        inRange[0].locationDateTime.getTime());
+    });
+
+  });
+
+});
+
