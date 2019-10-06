c1dccedce62fea847aed0999e6dfd8594114b080
rimu boddy <rimu@orbica.world>
2019-10-07 12:07:19 +1300

    fix siteservice

--- src/site/site.service.ts
+++ src/site/site.service.ts
@@ -45,26 +45,29 @@ export class SiteService {
       WHERE id NOT IN (${excludedIds.length ? excludedIds.join(",") : 0})
         AND company_id = ${companyId}
       ORDER BY
-        ST_DISTANCE(
-          site.geom,
-          ST_MakePoint(${lon}, ${lat})
-        )
-        ASC
+      ST_Distance_Sphere(
+        ST_SetSRID(site.geom, 4326),
+        ST_SetSRID(ST_MakePoint(${lon}, ${lat}), 4326)
+      )
+      ASC
       LIMIT 1;
     `;
     const res:any = await this.SITE_REPOSITORY.sequelize.query(sql, { raw: true });
-    return res[0].length ? res[0][0]['id'] : null;
+    return res[0].length ? parseInt(res[0][0]['id']) : null;
   }
 
   async getDistanceToSite(siteId: number, lat: number, lon: number): Promise<any> {
     const sql = `
-      SELECT ST_DISTANCE(site.geom, ST_MakePoint(${lon}, ${lat})) as id
+    SELECT
+        ST_Distance_Sphere(
+          ST_SetSRID(site.geom, 4326),
+          ST_SetSRID(ST_MakePoint(${lon}, ${lat}), 4326)
+      ) AS distance_meters
       FROM site where site.id = ${siteId}
       LIMIT 1;
     `
     const res:any = await this.SITE_REPOSITORY.sequelize.query(sql, { raw: true });
-    // tslint:disable-line
-    return res[0].length ? res[0][0]['id'] : null;
+    return res[0].length ? parseFloat(res[0][0]['distance_meters']) : null;
   }
 
 }
