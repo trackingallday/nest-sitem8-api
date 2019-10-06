b5546d081508cb18e5a448db04cbc8484e92326d
rimu boddy <rimu@orbica.world>
2019-10-07 12:05:22 +1300

    import env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+dotenv.config({path: path.resolve(__dirname + '../../.env.dev') });
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CompanyGuardInterceptor } from './common/companyGuard.interceptor';
 
 export async function bootstrap() {
+  console.log(path.resolve(__dirname + '../.env.dev'))
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new CompanyGuardInterceptor());
   await app.listen(3000);
