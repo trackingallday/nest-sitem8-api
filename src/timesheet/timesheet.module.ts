a743be8824fa4799d9c2c0d3da5d3bc3a9e6011e
rimu boddy <rimu@orbica.world>
2019-10-07 12:07:45 +1300

    fix imports

--- src/timesheet/timesheet.module.ts
+++ src/timesheet/timesheet.module.ts
@@ -4,20 +4,19 @@ import { TimesheetController } from './timesheet.controller';
 import { TimesheetService } from './timesheet.service';
 import { TimesheetProvider } from './timesheet.provider';
 import { DatabaseModule } from '../db/database.module';
+import { AccessTokenModule } from '../accessToken/accessToken.module';
+import { WorkerModule } from '../worker/worker.module';
+import { SiteModule } from '../site/site.module';
 import { AccessTokenService } from '../accessToken/accessToken.service';
 import { WorkerService } from '../worker/worker.service';
 import { SiteService } from '../site/site.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AccessTokenModule, WorkerModule, SiteModule],
   controllers: [TimesheetController],
   providers: [
-    TimesheetService,
-    AccessTokenService,
-    WorkerService,
-    SiteService,
-
     ...TimesheetProvider,
+    TimesheetService,
   ],
 })
 export class TimesheetModule {}
