8b096dbee0139ab3a85eda9171b98f221c5a0a88
rimu boddy <rimu@orbica.world>
2019-10-07 12:08:04 +1300

    fix th eloc tests

--- test/locationTimestamp.controller.spec.ts
+++ test/locationTimestamp.controller.spec.ts
@@ -5,11 +5,12 @@ import { DatabaseModule } from '../src/db/database.module';
 import { mockPost, mockGet } from './httpUtils';
 import testconstants from './test-constants';
 import { genLocationTimestamp } from './dataGenerators';
+import * as moment from 'moment';
 
 const expectedDistance0 = 0;
-const expectedDistance1 = 34.63897074;
-const expectedDistance2 = 32.34881906;
-const expectedDistance3 = 17.6417664;
+const expectedDistance1 = 18;
+const expectedDistance2 = 32;
+const expectedDistance3 = 35;
 
 <<<<<<< HEAD
 =======
@@ -29,18 +30,26 @@ describe('tests the LocationTimestampController', () => {
   describe('CRUD for locs', () => {
 
     it('creates some locations testing (also tests site service -> getClosestSite and getDistanceToSite)', async () => {
+
       const locs = testconstants.nearOrbicaPoints.map((c, i) => {
         return genLocationTimestamp(testconstants.device.deviceId, c[1], c[0], i);
       });
+
       const data = await Promise.all(locs.map(
         async l => await locController.create(
           mockPost('/locationtimestamp', l, { companyId: 1, id: 1 }), l)));
 
-      expect(data.map(d => d.closestSiteId).every(i => i === 1)).toBeTruthy();
-      expect(data[0].closestSiteDistance).toBe(expectedDistance0);
-      expect(data[1].closestSiteDistance).toBe(expectedDistance1);
-      expect(data[2].closestSiteDistance).toBe(expectedDistance2);
-      expect(data[3].closestSiteDistance).toBe(expectedDistance3);
+      const dsorted = data.sort((a, b) => {
+        return a.closestSiteDistance < b.closestSiteDistance ? -1 : 1;
+      });
+
+      console.log(dsorted);
+      expect(dsorted.map(d => d.closestSiteId).every(i => i === 1)).toBeTruthy();
+
+      expect(Math.round(dsorted[0].closestSiteDistance)).toBe(expectedDistance0);
+      expect(Math.round(dsorted[1].closestSiteDistance)).toBe(expectedDistance1);
+      expect(Math.round(dsorted[2].closestSiteDistance)).toBe(expectedDistance2);
+      expect(Math.round(dsorted[3].closestSiteDistance)).toBe(expectedDistance3);
     });
 
     it('gets the latest timestamps for the workers', async () => {
