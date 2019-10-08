a29de6499cc1ac3a4cfe23a9b39357f65df74490
rimu boddy <rimu@orbica.world>
2019-10-08 14:50:18 +1300

    Update notification.service.ts

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,11 +1,11 @@
 
 import { Injectable, Inject } from '@nestjs/common';
+import { Op } from 'sequelize';
 import { Notification } from './notification.entity';
 import { NotificationInterface } from './notification.interface';
 import { NotificationConstants, NotificationStatus } from './constants';
 import { LocationTimestamp } from '../locationTimestamp/locationTimestamp.entity';
 import { Worker } from '../worker/worker.entity';
-import { isEmpty, isNil } from 'lodash';
 import * as moment from 'moment';
 
 @Injectable()
@@ -33,10 +33,10 @@ export class NotificationService {
     return await this.NOTIFICATION_REPOSITORY.findByPk<Notification>(id);
   }
 
-  async getNotifications(filterWorkerId: number = null): Promise<Notification[]> {
+  async getNotifications(workerId: number = null): Promise<Notification[]> {
     return await this.NOTIFICATION_REPOSITORY.findAll<Notification>(
       {
-        where: { workerId: filterWorkerId },
+        where: { workerId },
         limit: NotificationConstants.MaximumRecords,
       },
     );
@@ -46,7 +46,7 @@ export class NotificationService {
     return await this.NOTIFICATION_REPOSITORY.findAll<Notification>(
       {
         where: {
-          $or: [
+          [Op.or]: [
             { smsStatus: { $like: NotificationStatus.Pending } },
             { emailStatus: { $like: NotificationStatus.Pending } },
           ],
@@ -61,7 +61,7 @@ export class NotificationService {
   }
 
   async updateOne(id: number, props: any): Promise<Notification> {
-    const notification = await this.findById(props);
+    const notification = await this.findById(id);
     notification.set(props);
     await notification.save();
     return notification;
@@ -75,9 +75,9 @@ export class NotificationService {
     return await this.NOTIFICATION_REPOSITORY.findOne<Notification>(
       {
         where: {
-          $and: {
+          [Op.and]: {
             workerId ,
-            smsSentDateTime : { $not: null },
+            smsSentDateTime : { [Op.ne]: null },
           },
         },
         order: [ 'smsSentDateTime' ],
@@ -86,89 +86,56 @@ export class NotificationService {
     );
   }
 
-  async addNotification(workerId: number, category: string, description: string, worker: Worker = null, mustEmail: boolean = false,
-                        fakeSmsSent: boolean = false)
-    : Promise<void> {
-    if (worker == null) {
-      // todo throw sitemate exception
-      // throw new SiteM8Exception("Worker not found with ID of " + workerId);
-    }
+  async addNotification(category: string, description: string, worker: Worker)
+    : Promise<Notification> {
     // Create the notification.
-    const notification: Notification = new Notification({
-      workerId,
+    const notification = {
+      workerId: worker.id,
       category,
-      description : description.length <= 160 ? description : description.substring(0, 160),
-      emailStatus : NotificationStatus.NotSent,
-      smsStatus : NotificationStatus.NotSent,
-      emailAddress : ``,
-      mobileNumber : ``,
-    });
+      description: description.length <= 160 ? description : description.substring(0, 160),
+      smsStatus: worker.mobileNotifications ? NotificationStatus.Pending : NotificationStatus.NotSent,
+      emailStatus: worker.email ? NotificationStatus.Pending : NotificationStatus.NotSent,
+      emailAddress: worker.email && worker.email,
+      mobileNumber: worker.mobile && worker.mobile,
+    };
 
-    // Certain notifications must be emailed regardless of whether the user has opted for email notifications.
-    if (worker.emailNotifications || mustEmail) {
-      if (isNil(worker.email) || isEmpty(worker.email)) {
-        // throw exception
-        // 	throw new SiteM8Exception("User does not have email address, yet requires a mandatory email notification");
-      }
-      notification.emailAddress = worker.email;
-      notification.emailStatus = NotificationStatus.Pending;
-    }
-
-    if (worker.mobileNotifications) {
-      notification.mobileNumber = worker.mobile;
-      notification.smsStatus = NotificationStatus.Pending;
-    }
-
-    // Needed for testing.
-    if (fakeSmsSent) {
-      notification.smsSentDateTime = new Date(2000, 1, 10);
-      notification.smsStatus = NotificationStatus.Sent;
-    }
-    this.NOTIFICATION_REPOSITORY.create(notification);
+    return await this.NOTIFICATION_REPOSITORY.create(notification);
   }
 
-  async addPasswordResetNotification(workerId: number, url: string, worker: Worker): Promise<void> {
-    if (url == null) {
-      // Todo: sitemate exception here
-    }
+  async addPasswordResetNotification(worker: Worker, url: string): Promise<void> {
     const message: string = `To reset your SiteM8 password: ${url}`;
-    this.addNotification(workerId, NotificationConstants.CategoryPasswordReset, message, worker, true);
+    this.addNotification(NotificationConstants.CategoryPasswordReset, message, worker);
   }
 
-  async addWelcomeNotification(workerId: number, url: string, worker: Worker): Promise<void> {
+  async addWelcomeNotification(url: string, worker: Worker): Promise<void> {
     const message: string = `Welcome to SiteM8. To set your password: ${url}`;
-    this.addNotification(workerId, NotificationConstants.CategoryPasswordReset, message, worker, true);
+    this.addNotification(NotificationConstants.CategoryPasswordReset, message, worker);
   }
 
-  async addDeviceNotification(workerId: number, worker: Worker): Promise<void> {
+  async addDeviceNotification(worker: Worker): Promise<void> {
     const message: string = `Please check your SiteM8 device is charging.`;
-    this.addNotification(workerId, NotificationConstants.CategoryPasswordReset, message, worker);
+    this.addNotification(NotificationConstants.CategoryDevice, message, worker);
   }
 
-  async addSosNotification(workerId: number, workerNeedingHelp: Worker, locationTimestamp: LocationTimestamp,
-                           sitename: string, worker: Worker): Promise<void> {
+  async addSosNotification(user: Worker, workerNeedingHelp: Worker, locationTimestamp: LocationTimestamp,
+                           sitename: string): Promise<void> {
     const when: Date = locationTimestamp.creationDateTime;
     let message: string = `SOS button activated for ${workerNeedingHelp.name} at ${moment(when).format('ddd HH:mm')}`;
 
     if (locationTimestamp.closestSiteDistance != null) {
-      try {
         message += ` at ${sitename}`;
-      } catch (Exception) {
-        // todo : logger
-        // logger.Error("Exception caught trying to append the site name for SOS notification");
-      }
     }
-    this.addNotification(workerId, NotificationConstants.CategorySOS, message, worker);
+    this.addNotification(NotificationConstants.CategorySOS, message, user);
 
     message = `Last known location at ${moment(locationTimestamp.locationDateTime).format('ddd HH:mm')} is ` +
     `https://www.google.com/maps/search/?api=1&query=${locationTimestamp.latitude},${locationTimestamp.longitude}`;
 
-    this.addNotification(workerId, NotificationConstants.CategorySOS, message, worker);
+    this.addNotification(NotificationConstants.CategorySOS, message, user);
   }
 
   // TODO send worker specific notifications to the correct supervisor
   // TODO send admin a notification for all the workers
-  async addAttendanceNotification(supervisorId: number, onSiteWorkers: Worker[], offSiteWorkers: Worker[]): Promise<void> {
+  async addAttendanceNotification(supervisor: Worker, onSiteWorkers: Worker[], offSiteWorkers: Worker[]): Promise<void> {
     const total: number = onSiteWorkers.length + offSiteWorkers.length;
 
     let message: string = `${onSiteWorkers.length} out of ${total} workers have arrived today.`;
@@ -178,35 +145,29 @@ export class NotificationService {
         message += ', ' + worker.name;
       });
     }
-    this.addNotification(supervisorId, NotificationConstants.CategoryAttendance, message);
-  }
-  async addSosAcknowledgementNotification(workerId: number, count: number) {
-    const message: string = `${count} ${count > 1 ? 'people' : 'person'} notified about your SiteM8 help request.`;
-    this.addNotification(workerId, NotificationConstants.CategorySOSConfirmation, message);
+    this.addNotification(NotificationConstants.CategoryAttendance, message, supervisor);
   }
 
-  async addTimesheetDailyNotification(workerId: number, date: Date, hours: number, minutes: number, websiteForWorkers: string) {
+  async addSosAcknowledgementNotification(user: Worker, count: number) {
+    const message: string = `${count} ${count > 1 ? 'people' : 'person'} notified about your SiteM8 help request.`;
+    this.addNotification(NotificationConstants.CategorySOSConfirmation, message, user);
+  }
+
+  async addTimesheetDailyNotification(worker: Worker, date: Date, hours: number, minutes: number, websiteForWorkers: string) {
     const message: string = `Your SiteM8 timesheet for ${moment(date).format('ddd')} is ${hours} hours ${minutes} min.` +
       `For any concerns please reply or visit ${websiteForWorkers}`;
-    this.addNotification(workerId, NotificationConstants.CategoryTimesheet, message);
+    this.addNotification(NotificationConstants.CategoryTimesheet, message, worker);
   }
 
-  async addApprovalReminderNotification(workerId: number, timesheetCount: number, websiteForSupervisors: string) {
+  async addApprovalReminderNotification(worker: Worker, timesheetCount: number, websiteForSupervisors: string) {
     const message: string = `There are ${timesheetCount} timesheets requiring approval at ${websiteForSupervisors}`;
-    this.addNotification(workerId, NotificationConstants.CategoryApprovalReminder, message);
+    this.addNotification(NotificationConstants.CategoryApprovalReminder, message, worker);
   }
 
-  async addTimesheetWeeklyNotification(workerId: number, date: number, hours: number, minutes: number, websiteForWorkers: string) {
+  async addTimesheetWeeklyNotification(worker: Worker, date: number, hours: number, minutes: number, websiteForWorkers: string) {
     const message: string = `Your SiteM8 timesheet for week ending ${moment(date).format('d MMM')} is ${hours} hours ${minutes} min.` +
      `For any concerns please reply or visit ${websiteForWorkers}`;
-    this.addNotification(workerId, NotificationConstants.CategoryTimesheet, message);
-  }
-
-  // todo:  GetLink(int workerId)
-
-  async addTestSmsSentNotification(workerId: number) {
-    const message: string = 'Test notification';
-    this.addNotification(workerId, NotificationConstants.CategoryDevice, message, null, false, true);
+    this.addNotification(NotificationConstants.CategoryTimesheet, message, worker);
   }
 
 }
