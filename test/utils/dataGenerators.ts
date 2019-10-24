f0a5e84c090491e76604b42ab2a1cccac5f32d8a
rimu boddy <rimu@orbica.world>
2019-10-24 22:47:16 +0000

    move utils to a folder

---
+++ test/utils/dataGenerators.ts
@@ -0,0 +1,30 @@
+
+
+export function genLocationTimestamp(deviceId: string, lat: number, lon: number, index: number, d?:Date) {
+  const date = new Date();
+  date.setMinutes(new Date().getMinutes() - (index + 10))
+  return {
+     deviceId: deviceId,
+     creationDateTime: date,
+     latitude: lat,
+     longitude: lon,
+     battery: 25,
+     locationDateTime: d || date,
+     rawData: `MT;6;${deviceId};R13;190507225204+00:83:8a:8a:20:71,-221,bb:2d:78:8a:20:71,-221+5,422275,28151,530,29059+3+4219+184`,
+     charging: false,
+     sosButton: false,
+     altitude: 10,
+     geom: {
+      type: 'Point',
+      coordinates: [
+          lon,
+          lat,
+      ],
+    },
+    companyId: 1,
+    closestSiteId: undefined,
+    closestSiteDistance: undefined,
+    workerId: undefined,
+  }
+}
+
