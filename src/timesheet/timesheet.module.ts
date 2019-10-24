c9bd533c0c97c10feba449a637022ff22c5a343d
rimu boddy <rimu@orbica.world>
2019-10-24 00:01:51 +0000

    fix imports for the timesheet module

--- src/timesheet/timesheet.module.ts
+++ src/timesheet/timesheet.module.ts
@@ -2,21 +2,21 @@
 import { Module } from '@nestjs/common';
 import { TimesheetController } from './timesheet.controller';
 import { TimesheetService } from './timesheet.service';
+import { TimesheetEntryModule } from '../timesheetEntry/timesheetEntry.module';
 import { TimesheetProvider } from './timesheet.provider';
 import { DatabaseModule } from '../db/database.module';
-import { AccessTokenModule } from '../accessToken/accessToken.module';
+import { LocationTimestampModule } from '../locationTimestamp/locationTimestamp.module';
 import { WorkerModule } from '../worker/worker.module';
 import { SiteModule } from '../site/site.module';
-import { AccessTokenService } from '../accessToken/accessToken.service';
-import { WorkerService } from '../worker/worker.service';
-import { SiteService } from '../site/site.service';
+
 
 @Module({
-  imports: [DatabaseModule, AccessTokenModule, WorkerModule, SiteModule],
+  imports: [DatabaseModule, WorkerModule, SiteModule, TimesheetEntryModule, LocationTimestampModule],
   controllers: [TimesheetController],
   providers: [
     ...TimesheetProvider,
     TimesheetService,
   ],
+  exports: [TimesheetService]
 })
 export class TimesheetModule {}
