fa1a79a00dbb5f17b4fe6e0a98c4237e05edfe46
rimu boddy <rimu@orbica.world>
2019-10-24 00:00:40 +0000

    don't use the 'rawdb'

--- src/common/rawdatabase.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize-typescript';
-import constants from '../constants';
-
-const { DBDATABASE, DBPASSWORD, DBPROVIDE, DBPOSTGRESUSERNAME, DBDIALECT, DBHOST, DBPORT } = process.env;
-
-export default new Sequelize(DBDATABASE, DBPOSTGRESUSERNAME, DBPASSWORD, {
-  dialect: 'postgres',
-});
-
