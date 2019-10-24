d87f3d3e95bfa8506f516a4c944be3235fda2973
rimu boddy <rimu@orbica.world>
2019-10-24 23:47:37 +0000

    mock axios so dont hit Auth0 each time we test

--- test/auth0.gateway.spec.ts
+++ test/auth0.gateway.spec.ts
@@ -1,9 +1,19 @@
 
-import Auth0Gateway from '../src/common/auth0.gateway';
-
+import Auth0Gateway, { baseUrl } from '../src/common/auth0.gateway';
+import axios from 'axios';
 
 const email = Math.random() + 'rimu.aw23.lol@chickens.org';
 const email2 = Math.random() + 'dave.aw3s.lol@chickens.org';
+
+const mockAxiosPost = jest.fn();
+const mockAxiosDelete = jest.fn();
+const mockAxiosPatch = jest.fn();
+
+
+jest.mock('axios');
+axios.patch.mockResolvedValue({ data: { ticket: 'a', access_token: 'c' }});
+axios.post.mockResolvedValue({ data: { ticket: 'a', access_token: 'c' }});
+axios.delete.mockResolvedValue({ data: { ticket: 'a', access_token: 'c' }})
 
 
 describe('auth0 user methods', () => {
@@ -11,20 +21,20 @@ describe('auth0 user methods', () => {
   const gway = new Auth0Gateway();
   it('creates a user', async () => {
     const data = await gway.createUser(email);
-    authId = data.user_id;
-    expect(authId).not.toBe(undefined);
-    expect(data.email).toBe(email);
+    expect(axios.post).toBeCalledTimes(2);
   });
   it('resets user password', async () => {
     const data = await gway.resetUserPassword(authId);
-    expect(data).toContain('reset?ticket');
+    expect(axios.post).toBeCalledTimes(4);
   });
   it('updates a user', async () => {
     const data = await gway.updateUser({ email: email2, authId });
-    expect(data.email).toBe(email2);
+    expect(axios.patch).toBeCalledTimes(1);
+    expect(axios.post).toBeCalledTimes(5);
   });
   it('deletes a user', async () => {
-    const data = gway.deleteUser(authId);
-    expect(typeof data).toBe('object');
+    const data = await gway.deleteUser(authId);
+    expect(axios.delete).toBeCalledTimes(1);
+    expect(axios.post).toBeCalledTimes(6);
   });
 });
