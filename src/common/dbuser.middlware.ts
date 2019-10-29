8dac0fe63c489b0b736396b94e7bb32b2a9d5572
rimu-at-307937472747 <rimuboddy@Rimus-iMac.local>
2019-10-30 12:04:55 +1300

    fix dbuser middleware

--- src/common/dbuser.middlware.ts
+++ src/common/dbuser.middlware.ts
@@ -1,16 +1,20 @@
-import { NestMiddleware } from '@nestjs/common';
+import { NestMiddleware, Injectable } from '@nestjs/common';
 import { WorkerService } from '../worker/worker.service';
+import { Company } from '../company/company.entity';
 
-
+@Injectable()
 export class DbUserMiddleware implements NestMiddleware {
 
-  constructor(private readonly workerService: WorkerService) {}
+  constructor(private readonly workerService: WorkerService) {
+    this.workerService = workerService;
+  }
 
   //req is an express object - but we use any to make typscript not flip out
   async use(req:any, res: Response, next:Function) {
     if(req.user) {
-      const params = { authId: req.user.sub };
-      const dbUser = await this.workerService.findOneWhere({ where: params });
+      const params = { where : { authId: req.user.sub }, include: [Company] };
+      const dbUser = await this.workerService.findOneWhere(params);//auth0|5be35d20d6269e68e95b2a00                 
+      //const dbUser = await this.workerService.findAll();//auth0|5be35d20d6269e68e95b2a00                 
       if(!(dbUser && dbUser.isEnabled)) {
         throw new Error('Not Authorised');
       }
